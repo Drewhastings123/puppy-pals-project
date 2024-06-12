8fb95d2bcbe8464b833decb61376ab790a6972ea
Drew Hastings <drewhastings@Drews-MacBook-Pro.local>
2024-06-12 13:15:38 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  // console.log(puppies);
   return (
     <>
-      <div></div>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+            </div>
+        )}
+      </div>
     </>
   );
 }
